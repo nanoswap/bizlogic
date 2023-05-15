673cc29ecee97caa65b04e931ce23325964f6eb5
Nate Schultz <nate.schultz@outlook.com>
2023-05-15 13:23:54 -0400

    loan status lint fixes

--- bizlogic/loan/status.py
+++ bizlogic/loan/status.py
@@ -1,28 +1,46 @@
-
-from google.protobuf.timestamp_pb2 import Timestamp
-from enum import Enum
 from datetime import datetime, timedelta
+from enum import Enum
 
 from bizlogic.protoc.loan_pb2 import Loan
 
+from google.protobuf.timestamp_pb2 import Timestamp
+
 
 class LoanStatusType(Enum):
+    """Loan Status Type."""
+
     PENDING_ACCEPTANCE = 1
     EXPIRED_UNACCEPTED = 2
     ACCEPTED = 3
 
 
 class LoanStatus():
+    """Loan Status."""
 
     @staticmethod
     def _timestamp_to_datetime(timestamp: Timestamp) -> datetime:
+        """Convert a protobuf timestamp to a datetime.
+
+        Args:
+            timestamp: the timestamp
+
+        Returns:
+            datetime: the datetime
+        """
         seconds = datetime.fromtimestamp(timestamp.seconds)
         micros = timedelta(microseconds=timestamp.nanos / 1000)
         return seconds + micros
 
-
     @staticmethod
-    def loan_status(loan: Loan):
+    def loan_status(loan: Loan) -> LoanStatusType:
+        """Get the status of a loan.
+
+        Args:
+            loan: the loan
+
+        Returns:
+            LoanStatusType: the status of the loan
+        """
         now = datetime.now()
         expiry = LoanStatus._timestamp_to_datetime(loan.offer_expiry)
 
